1aff531d8a752a0ff810cbe19bbe16d249de21d8
Valerio Tognozzi <vtogn@DESKTOP-9G886DV.localdomain>
2022-11-21 14:38:35 +0100

    comincio a leggere i dati JSON

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var gamesRouter = require('./routes/games');
 
 var app = express();
 
@@ -19,8 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/games', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -8,36 +8,80 @@ const apiKey = require('../.env');
 var config = {
   method: 'get',
   url: `https://v1.rugby.api-sports.io/games?date=${oggi}`,
-  //url: `https://v1.rugby.api-sports.io/games?date=2022-11-18`,
   headers: {
     'x-rapidapi-key':apiKey.apiKey,
     'x-rapidapi-host': 'v1.rugby.api-sports.io'
   }
 };
 
-// *******registro su file*******
-const fs = require('fs')
-
-const storeData = (data, path) => {
-  try {
-    fs.writeFileSync(path, JSON.stringify(data))
-  } catch (err) {
-    console.error(err)
-  }
-}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /* 
+  // TODO spostrare la funzione in service
   axios(config)
   .then(function (response) {
-    //salvo il json ricevuto su  file sul disco
-    //storeData(response.data, 'ilMioFile.json');
     res.render('index', { title: 'Rugby Challenge', data: JSON.stringify(response.data) });
   })
   .catch(function (error) {
     console.log(error);
   });
-  //res.render('index', { title: 'Rugby Challenge', data: JSON.stringify(response.data) });
-});
+   */
+  res.render(
+    'index', 
+    { 
+      title: 'Rugby Challenge',
+      data: {
+        parameters:{date:"2019-11-11"},
+        results: 2,
+        response: [
+          {
+            id:22, 
+            date: "2018-11-13 12:30",
+            week: "Quarter-finals",
+            country:{
+              id:26,
+              name: "World"
+            },
+            teams:{
+              home:{
+                id: 132,
+                name: "Monaco 7s",
+                logo: "https://media.api-sports.io/rugby/teams/132.png"
+              },
+              away:{
+                id: 130,
+                name: "La Rochelle 7s",
+                logo: "https://media.api-sports.io/rugby/teams/130.png",
+                flag: null
+              }
+            }  
+          },
+          {
+            id:23, 
+            date: "2018-11-14 12:30",
+            week: "null",
+            country:{
+              id:9,
+              name: "Germany",
+              flag: "https://media.api-sports.io/flags/de.svg"
+            },
+            teams:{
+              home:{
+                id: 165,
+                name: "RK 03 Berlin",
+                logo: "https://media.api-sports.io/rugby/teams/165.png"
+              },
+              away:{
+                id: 160,
+                name: "Leipzig",
+                logo: "https://media.api-sports.io/rugby/teams/160.png"
+              }
+            }  
+          }
+        ]
+            }
+          });
+      });
 
 module.exports = router;
